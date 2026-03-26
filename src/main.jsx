8b3dcc7f0cfb12a144ccd9922e5caf1eb83b07f7
nervenet450-hub <nervenet450@gmail.com>
2026-03-26 18:27:36 +0200

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import ClearViewDental from "./ClearViewDental.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <ClearViewDental />
-  </StrictMode>
-);
